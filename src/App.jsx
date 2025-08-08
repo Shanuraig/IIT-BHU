fb33ba41d0d14bad4f4a70777d91f33acc3b6d16
Gyan Prakash Rai <shanuraig@gmail.com>
2025-08-09 04:43:34 +0530

    Update import paths to use capitalized folder names
    Changed all import statements to use 'Components' and 'Pages' with capitalized folder names for consistency. Also fixed the '/Visits' route path to '/visits' and removed the unused Hero component route.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import Navbar from './Components/Navbar';
 
 // Page or section components
-import Home from './pages/Home';
-import AreaOfInterest from './components/AreaOfInterest';
-import Distinctions from './components/Distinctions';
-import Facilities from './components/Facilities';
-import GroupActivities from './components/GroupActivities';
-import GroupMembers from './components/GroupMembers';
-import Hero from './components/Hero';
-import Visits from './components/Visits';
-import Publications from './components/Publications';
-import SelectedPublications from './components/SelectedPublications';
-import Projects from './components/Projects';
-import Positions from './components/Positions';
-import Collaborations from './components/Collaborations';
+import Home from './Pages/Home';
+import AreaOfInterest from './Components/AreaOfInterest';
+import Distinctions from './Components/Distinctions';
+import Facilities from './Components/Facilities';
+import GroupActivities from './Components/GroupActivities';
+import GroupMembers from './Components/GroupMembers';
+import Visits from './Components/Visits';
+import Publications from './Components/Publications';
+import SelectedPublications from './Components/SelectedPublications';
+import Projects from './Components/Projects';
+import Positions from './Components/Positions';
+import Collaborations from './Components/Collaborations';
+
 const App = () => {
   return (
     <>
@@ -27,8 +27,7 @@ const App = () => {
         <Route path="/facilities" element={<Facilities />} />
         <Route path="/group-activities" element={<GroupActivities />} />
         <Route path="/group-members" element={<GroupMembers />} />
-        <Route path="/hero" element={<Hero />} />
-        <Route path="/Visits" element={<Visits />} />
+        <Route path="/visits" element={<Visits />} />
         <Route path="/positions" element={<Positions />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/publications" element={<Publications />} />
